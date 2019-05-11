ae052037174a8393f45238847b78724edba2a047
Donald Yeh <Donald.Yeh@gmail.com>
2019-05-11 15:22:32 -0400

    adds a heart for users to love <3

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 // import { Link } from "react-router";
 
-export default function Post() {
-	return (
-		<div>
-			<h1>this is the post component</h1>
-			<figure>
-				<div>
-					<Link to="#">
-						<img alt="cute Boone pic goes here" />
-					</Link>
-				</div>
-				<figcaption>figure caption here</figcaption>
-			</figure>
-		</div>
-	);
+export default class Post extends Component {
+	render() {
+		return (
+			<div>
+				<h1>this is the post component</h1>
+				<figure>
+					<div>
+						<Link to="#">
+							<img alt="cute Boone pic goes here" />
+						</Link>
+						<CSSTransition
+							transitionName="like"
+							transitionEnterTimeout={500}
+							transitionLeaveTimeout={500}
+						>
+							<span className="likes-heart">1</span>
+						</CSSTransition>
+					</div>
+					<figcaption>figure caption here</figcaption>
+				</figure>
+			</div>
+		);
+	}
 }
