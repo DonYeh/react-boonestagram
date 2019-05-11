49bfcef5703515dcf03d0379723c6e5c8bf3c8e8
Donald Yeh <Donald.Yeh@gmail.com>
2019-05-11 14:49:49 -0400

    imports react-router-dom to use Link for photos

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// import { Link } from "react-router";
 
 export default function Post() {
 	return (
 		<div>
 			<h1>this is the post component</h1>
+			<figure>
+				<div>
+					<Link to="#">
+						<img alt="cute Boone pic goes here" />
+					</Link>
+				</div>
+				<figcaption>figure caption here</figcaption>
+			</figure>
 		</div>
 	);
 }
